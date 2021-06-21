e5179572a9aecad6c0b81e6b9c7e6cc1d79f4bc8
Duck <yassirelyakine@gmail.com>
2021-06-21 17:22:29 +0100

    Added python directory with 2 files inside

---
+++ python/strings.py
@@ -0,0 +1,21 @@
+string = "I am a string."
+string_length = len(string) # store length of string in variable
+print("string length:", string_length)
+
+# convert string to lowercase
+print("Lowercase:", string.lower())
+
+# convert string to uppercase
+print("Uppercase: ", string.upper())
+
+# check if string is uppercase
+print(f"is ({string}) uppercase?:", string.isupper())
+
+# check if string is lowercase
+print(f"is ({string}) lowercase?:", string.islower())
+
+# return index of character
+print("index(\"I\") =", string.index("I"))
+
+# replace list of characters
+print(string.replace("I am", "You are"))
