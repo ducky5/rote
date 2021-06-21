6e29ca4951f99ccb3c27ae1e337689b1257cbe4c
Duck <yassirelyakine@gmail.com>
2021-06-21 16:01:16 +0100

    finished objects.js(for now)

--- js/objects.js
+++ js/objects.js
@@ -14,3 +14,22 @@ console.log("]0;3]: " + numbers.excluded_0_3included)
 
 // accessing object values with [] notation: object["property"]
 console.log("]0;1]: " + numbers["one"])
+
+// add a new property to object using (.) notation: object.new_property
+numbers[1] = "one"
+console.log(numbers)
+
+// delete property from object
+delete numbers[1]
+console.log(numbers)
+
+// function to check if object has property
+function hasProperty(object, property) {
+  if (object.hasOwnProperty(property)) {
+    return "object has property \"" + property + "\""
+  }
+  else {
+    return "object does not have property \"" + property + "\""
+  }
+}
+console.log(hasProperty(numbers, "one"))
