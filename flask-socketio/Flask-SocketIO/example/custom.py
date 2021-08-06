472da03341d4408c7b906be4d6f8a2939819c98f
Duck <yassirelyakine@gmail.com>
2021-08-06 13:19:47 +0100

    modified custom.py in flask-socketio/

--- flask-socketio/Flask-SocketIO/example/custom.py
+++ flask-socketio/Flask-SocketIO/example/custom.py
@@ -10,14 +10,14 @@ socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 
-def background_thread():
-    """Example of how to send server generated events to clients."""
-    count = 0
-    while True:
-        socketio.sleep(10)
-        count += 1
-        socketio.emit('my_response',
-                      {'data': 'Server generated event', 'count': count})
+# def background_thread():
+#     """Example of how to send server generated events to clients."""
+#     count = 0
+#     while True:
+#         socketio.sleep(10)
+#         count += 1
+#         socketio.emit('my_response',
+#                       {'data': 'Server generated event', 'count': count})
 
 @app.route('/')
 def index():
