53c046c4b2a2849076a548e17dfe87c577c0af1b
hiro <yassirelyakine@gmail.com>
2022-03-27 14:39:54 +0000

    done with function_safe_fileio.py

--- python/function_safe_fileio.py
+++ python/function_safe_fileio.py
@@ -5,7 +5,7 @@ import pathlib
 def fileHandler(file_path, mode):
     print(f'\nopening file (mode={mode})...')
     file_obj = pathlib.Path(file_path).open(mode=mode)
-    print(f'file {file_obj.name} opened.\n')
+    print(f'file {file_obj.name} opened.')
     try:
         yield file_obj
     finally:
@@ -14,5 +14,11 @@ def fileHandler(file_path, mode):
             file_obj.close()
             print(f'file {file_obj.name} closed.')
 
+#
 with fileHandler('./data/write_filev2.txt', 'w') as file:
-    file.write('Version 2.0')
+    file.write('A'*99999999)
+
+with fileHandler('./data/write_filev2.txt', 'r') as file:
+    if file.readable():
+        # print(file.read().join(['"']*2))
+        pass
